f2c257a4b6682652126b36ab467d58d7a1f6cf25
Tymoniusz <tymanowski@gmail.com>
2019-12-17 21:08:31 +0100

    Dodanie do programu interfejsu graficznego GUI Dodanie: Button, Entry

--- Program.py
+++ Program.py
@@ -11,4 +11,41 @@
 
 ##c. stwórz wykres (obiektowość, podstawy pakietów do rysowania).
 
-print("Hello World")
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+
+def tylkocyfry(*a):
+    global tx,pop_s
+    s= tx.get()
+    if s == "" or s.isdigit():
+        pop_s = s
+    else:
+        tx.set(pop_s)
+
+def start():
+    global tx
+    Ok = False
+    wyjscie = tx.get()
+    messagebox.showinfo("Sukces",wyjscie)
+
+o = tk.Tk()
+o.geometry("310x50")
+o.title("Brutton")
+
+
+tk.Label(o, text="Kwota brutto (zł):").grid(row=0, column=0)
+
+tx = StringVar()
+pop_s = "" #moduł do wpisania samych cyfr
+tx.set(pop_s)
+tx.trace("w", tylkocyfry)
+
+
+tk.Entry(o,width=30, textvariable=tx).grid(row=0, column=1)
+tk.Button(o, text="Ok", command=start).grid(row=0, column=2)
+
+dana= ""
+
+
+o.mainloop()
