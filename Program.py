1554024a55681fca5564c77e69420096b9cd2a2a
Tymoniusz <tymanowski@gmail.com>
2019-12-17 19:32:21 +0100

    Dodano próbnie zadanie

--- Program.py
+++ Program.py
@@ -1,4 +1,18 @@
 Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
-print("hello world")
+
+##Zgodnie z naszą rozmową, przesyłam test. Ma on sprawdzić Twoje obecne umiejętności i umiejętność rozwiązywania problemów.
+
+##Podstawowe zadanie: Stwórz aplikację, która przy pomocy kalkulatora wynagrodzeń on-line wypisze kwoty netto przyporządkowane podanym kwotom brutto. 
+
+##Zadanie z gwiazdką: Stwórz wykres, który zilustruje zależność między kwotą brutto (oś X) a kwotą netto (oś Y) wynagrodzenia.
+
+##Podpowiedzi:
+
+##a. kwoty brutto podawane są jako argumenty aplikacji. Użyj minimum 6 wartości brutto (pętle, użycie pakietu sys, listy).
+
+##b. kwoty netto wylicz na podstawie witryny https://wynagrodzenia.pl/   (umiejętność korzystania z pakietów komunikujących się z serwerami www, obiektowość, obsługa błędów).
+
+##c. stwórz wykres (obiektowość, podstawy pakietów do rysowania).
+print("Hello World")
